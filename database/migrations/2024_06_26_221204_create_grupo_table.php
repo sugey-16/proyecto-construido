6f4a3ea7f01ed59857f7acfc1f1bf67e7a08f49a
mady1234567817 <mady1234567817@outlook.com>
2024-07-05 16:39:11 -0600

    proyecto final

---
+++ database/migrations/2024_06_26_221204_create_grupo_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('grupo', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 255)->nullable(false);
+            $table->string('descripcion', 255)->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('grupo');
+    }
+};
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        DB::table('docente')->insert([
+            'nombre' => 'admin',
+            'apellido' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin'),
+        ]);
+    }
+
+}
